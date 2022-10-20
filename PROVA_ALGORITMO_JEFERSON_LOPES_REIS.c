01389a37b8de1c975c3d95ec08b51491fcbe96e6
jef-loppes-reis <jefersondip25@gmail.com>
2022-10-19 21:32:43 -0300

    Questao A

---
+++ PROVA_ALGORITMO_JEFERSON_LOPES_REIS.c
@@ -0,0 +1,13 @@
+#include <std.io>
+
+int main()
+{
+
+    printf("\nDiciplina: Algoritmos e Programação");
+    printf("\nNome do Professor(a): Joyce Siqueira");
+    printf("\nNome do aluno: Jefeson Lopes Reis");
+    printf("\nMatricula: UC22200711");
+    printf("\nCurso: Ciencia da Computação");
+    printf("\nLink Repositorio GitHub: https://github.com/jef-loppes-reis/Prova_UCB_Algoritimos");
+    printf("\nIDE utilizada: VSCODE e DEVC++ (Para compilar)");
+}
